89039750c67c5a72ac89de878b1b0ef096ca6d91
Otavio Dutra <dutra.otavio@gmail.com>
2021-09-23 19:21:57 -0300

    padrão da data alterado

--- src/main/java/br/com/otavio/livraria/dto/LivroFormDto.java
+++ src/main/java/br/com/otavio/livraria/dto/LivroFormDto.java
@@ -9,6 +9,8 @@ import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import br.com.otavio.livraria.modelo.Autor;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,13 +22,17 @@ public class LivroFormDto {
           @NotBlank
           @Size(min = 10, max = 30)
           private String titulo;
+
           @NotNull
           @PastOrPresent
+          @JsonFormat(pattern = "dd/MM/yyyy")
           private LocalDate dataDeLancamento;
+
           @NotNull
           @Positive
           @DecimalMin(value = "100")
           private int paginas;
+
           @NotNull
           private Autor autor;
 
