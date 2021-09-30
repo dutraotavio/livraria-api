557d9f0d98d8ce90cf4aca52029ed71429c804b4
Otavio Dutra <dutra.otavio@gmail.com>
2021-09-30 19:40:05 -0300

    adicionando validação

--- src/main/java/br/com/otavio/livraria/dto/AutorFormDto.java
+++ src/main/java/br/com/otavio/livraria/dto/AutorFormDto.java
@@ -2,6 +2,14 @@ package br.com.otavio.livraria.dto;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,9 +17,20 @@ import lombok.Setter;
 @Setter
 public class AutorFormDto {
 
+          @NotBlank
           private String nome;
+
+          @NotBlank
+          @Email
           private String email;
+
+          @NotNull
+          @Past
+          @JsonFormat(pattern = "dd/MM/yyyy")
           private LocalDate dataDeNascimento;
+
+          @NotBlank
+          @Size(min = 10, max = 100)
           private String curriculo;
 
 }
